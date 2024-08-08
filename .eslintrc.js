c43c39e25b85afc4535239029c3b4af8df26f617
Nicolas Houlier <nicolas.houlier@gmail.com>
2024-08-08 10:49:00 +0200

    Update .eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,28 @@
 module.exports = {
-    env: {
-        browser: true,
-        es2021: true,
-    },
-    extends: ['airbnb-base'],
+    extends: [
+      'eslint:recommended',
+      'plugin:@typescript-eslint/eslint-recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:prettier/recommended'
+    ],
+    parser: '@typescript-eslint/parser',
     parserOptions: {
-        ecmaVersion: 13,
-        sourceType: 'module',
+      project: 'tsconfig.eslint.json',
+      sourceType: 'module'
     },
+    plugins: ['@typescript-eslint'],
     rules: {
-        indent: ['error', 4],
-        'no-console': 'off',
-        'no-param-reassign': 'off',
-    },
-};
+      '@typescript-eslint/no-unused-vars': ['warn', { args: 'none' }],
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-namespace': 'off',
+      '@typescript-eslint/no-use-before-define': 'off',
+      '@typescript-eslint/quotes': [
+        'error',
+        'single',
+        { avoidEscape: true, allowTemplateLiterals: false }
+      ],
+      curly: ['error', 'all'],
+      eqeqeq: 'error',
+      'prefer-arrow-callback': 'error'
+    }
+  };
