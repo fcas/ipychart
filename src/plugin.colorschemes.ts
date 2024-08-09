e2a89c55ef95d2b3e40e892b0fa0a9e8937b3c45
Nicolas Houlier <nicolas.houlier@gmail.com>
2024-08-09 17:04:21 +0200

    Move plugin colorscheme code to modern JS

--- src/plugin.colorschemes.ts
+++ src/plugin.colorschemes.ts
@@ -1,10 +1,10 @@
 // This code comes from the chartjs-plugin-colorschemes package.
 // The original code of this package is not compatible with chart.js 3.x.
-// Therefore, the code has been adapted to work with chart.js 3.x. and integrated to ipychart.
+// Therefore, the code has been adapted to work with chart.js 3.x and integrated into ipychart.
 // To see the original version of this file, please visit:
 // https://github.com/nagix/chartjs-plugin-colorschemes/blob/master/src/plugins/plugin.colorschemes.js
 
-import Chart from 'chart.js/auto';
+import Chart, { ChartDataset, Plugin, ChartConfiguration, ChartType } from 'chart.js/auto';
 import { isArray } from 'chart.js/helpers';
 
 const EXPANDO_KEY = '$colorschemes';
@@ -31,22 +31,25 @@ function addAlpha(hex: string): string {
     return `rgba(${r}, ${g}, ${b}, 0.5)`;
 }
 
-function getScheme(scheme: any) {
+function getScheme(scheme: string | any[]): any[] | null {
     if (isArray(scheme) || scheme === null) {
         return scheme;
     }
-    const colorschemes = (Chart as any).colorschemes || {}; // Cast 'Chart' as 'any' to avoid TypeScript error
-    const arr = scheme.split('.');
-    const category = colorschemes[arr[0]];
-    return category[arr[1]];
+    if (typeof scheme === 'string') {
+        const colorschemes = (Chart as any).colorschemes || {};
+        const arr = scheme.split('.');
+        const category = colorschemes[arr[0]];
+        return category[arr[1]];
+    }
+    return null;
 }
 
-const ColorSchemesPlugin = {
+const ColorSchemesPlugin: Plugin<
+    'line' | 'bar' | 'radar' | 'scatter' | 'doughnut' | 'pie' | 'polarArea'
+> = {
     id: 'colorschemes',
 
-    beforeUpdate(chart: any, args: any, options: any) {
-        // Please note that in v3, the args argument was added. It was not used before it was added,
-        // so we just check if it is not actually our options object
+    beforeUpdate(chart, args, options: any) {
         if (options === undefined) {
             options = args;
         }
@@ -62,99 +65,140 @@ const ColorSchemesPlugin = {
             length = scheme.length;
 
             // Set scheme colors
-            chart.config.data.datasets.forEach((dataset: any, datasetIndex: number) => {
-                // Added type annotations for 'dataset' and 'datasetIndex'
-                colorIndex = datasetIndex % length!;
-                colorCode = scheme[reverse ? length - colorIndex - 1 : colorIndex];
+            chart.config.data.datasets.forEach(
+                (dataset: ChartDataset<ChartType>, datasetIndex: number) => {
+                    colorIndex = datasetIndex % length;
+                    colorCode = scheme[reverse ? length - colorIndex - 1 : colorIndex];
 
-                // Object to store which color option is set
-                dataset[EXPANDO_KEY] = {};
+                    // Object to store which color option is set
+                    (dataset as any)[EXPANDO_KEY] = {};
 
-                switch (dataset.type || chart.config.type) {
-                    // For line, radar and scatter chart, borderColor and backgroundColor are set
-                    case 'line':
-                    case 'radar':
-                    case 'scatter':
-                        if (typeof dataset.backgroundColor === 'undefined' || override) {
-                            dataset[EXPANDO_KEY].backgroundColor = dataset.backgroundColor;
-                            dataset.backgroundColor = addAlpha(colorCode);
-                        }
-                        if (typeof dataset.borderColor === 'undefined' || override) {
-                            dataset[EXPANDO_KEY].borderColor = dataset.borderColor;
-                            dataset.borderColor = colorCode;
-                        }
-                        if (typeof dataset.pointBackgroundColor === 'undefined' || override) {
-                            dataset[EXPANDO_KEY].pointBackgroundColor =
-                                dataset.pointBackgroundColor;
-                            dataset.pointBackgroundColor = addAlpha(colorCode);
-                        }
-                        if (typeof dataset.pointBorderColor === 'undefined' || override) {
-                            dataset[EXPANDO_KEY].pointBorderColor = dataset.pointBorderColor;
-                            dataset.pointBorderColor = colorCode;
-                        }
-                        break;
-                    // For doughnut and pie chart, backgroundColor is set to an array of colors
-                    case 'doughnut':
-                    case 'pie':
-                    case 'polarArea':
-                        if (typeof dataset.backgroundColor === 'undefined' || override) {
-                            dataset[EXPANDO_KEY].backgroundColor = dataset.backgroundColor;
-                            dataset.backgroundColor = dataset.data.map(
-                                (data: any, dataIndex: number) => {
-                                    // Added type annotations for 'data' and 'dataIndex'
-                                    colorIndex = dataIndex % length!;
-                                    return scheme[reverse ? length - colorIndex - 1 : colorIndex];
+                    const chartType = (chart.config as ChartConfiguration).type;
+
+                    switch (dataset.type || chartType) {
+                        case 'line':
+                        case 'radar':
+                        case 'scatter':
+                            if (typeof dataset.backgroundColor === 'undefined' || override) {
+                                (dataset as any)[EXPANDO_KEY].backgroundColor =
+                                    dataset.backgroundColor;
+                                dataset.backgroundColor = addAlpha(colorCode);
+                            }
+                            if (typeof dataset.borderColor === 'undefined' || override) {
+                                (dataset as any)[EXPANDO_KEY].borderColor = dataset.borderColor;
+                                dataset.borderColor = colorCode;
+                            }
+                            // Ensure the dataset has point-specific properties
+                            if (
+                                dataset.type === 'line' ||
+                                dataset.type === 'radar' ||
+                                dataset.type === 'scatter'
+                            ) {
+                                const lineDataset = dataset as ChartDataset<
+                                    'line' | 'radar' | 'scatter'
+                                >;
+                                if (
+                                    typeof lineDataset.pointBackgroundColor === 'undefined' ||
+                                    override
+                                ) {
+                                    (dataset as any)[EXPANDO_KEY].pointBackgroundColor =
+                                        lineDataset.pointBackgroundColor;
+                                    lineDataset.pointBackgroundColor = addAlpha(colorCode);
                                 }
-                            );
-                        }
-                        break;
-                    // For bar chart backgroundColor (including fillAlpha) and borderColor are set
-                    case 'bar':
-                        if (typeof dataset.backgroundColor === 'undefined' || override) {
-                            dataset[EXPANDO_KEY].backgroundColor = dataset.backgroundColor;
-                            dataset.backgroundColor = colorCode;
-                        }
-                        if (typeof dataset.borderColor === 'undefined' || override) {
-                            dataset[EXPANDO_KEY].borderColor = dataset.borderColor;
-                            dataset.borderColor = colorCode;
-                        }
-                        break;
-                    // For the other chart, only backgroundColor is set
-                    default:
-                        if (typeof dataset.backgroundColor === 'undefined' || override) {
-                            dataset[EXPANDO_KEY].backgroundColor = dataset.backgroundColor;
-                            dataset.backgroundColor = colorCode;
-                        }
-                        break;
+                                if (
+                                    typeof lineDataset.pointBorderColor === 'undefined' ||
+                                    override
+                                ) {
+                                    (dataset as any)[EXPANDO_KEY].pointBorderColor =
+                                        lineDataset.pointBorderColor;
+                                    lineDataset.pointBorderColor = colorCode;
+                                }
+                            }
+                            break;
+
+                        case 'doughnut':
+                        case 'pie':
+                        case 'polarArea':
+                            if (typeof dataset.backgroundColor === 'undefined' || override) {
+                                (dataset as any)[EXPANDO_KEY].backgroundColor =
+                                    dataset.backgroundColor;
+                                dataset.backgroundColor = dataset.data.map(
+                                    (_: any, dataIndex: number) => {
+                                        colorIndex = dataIndex % length;
+                                        return scheme[
+                                            reverse ? length - colorIndex - 1 : colorIndex
+                                        ];
+                                    }
+                                );
+                            }
+                            break;
+
+                        case 'bar':
+                            if (typeof dataset.backgroundColor === 'undefined' || override) {
+                                (dataset as any)[EXPANDO_KEY].backgroundColor =
+                                    dataset.backgroundColor;
+                                dataset.backgroundColor = colorCode;
+                            }
+                            if (typeof dataset.borderColor === 'undefined' || override) {
+                                (dataset as any)[EXPANDO_KEY].borderColor = dataset.borderColor;
+                                dataset.borderColor = colorCode;
+                            }
+                            break;
+
+                        default:
+                            if (typeof dataset.backgroundColor === 'undefined' || override) {
+                                (dataset as any)[EXPANDO_KEY].backgroundColor =
+                                    dataset.backgroundColor;
+                                dataset.backgroundColor = colorCode;
+                            }
+                            break;
+                    }
                 }
-            });
+            );
         }
     },
 
-    afterUpdate(chart: any) {
+    afterUpdate(chart) {
         // Unset colors
-        chart.config.data.datasets.forEach((dataset: any) => {
-            if (dataset[EXPANDO_KEY]) {
-                if (Object.prototype.hasOwnProperty.call(dataset[EXPANDO_KEY], 'backgroundColor')) {
-                    dataset.backgroundColor = dataset[EXPANDO_KEY].backgroundColor;
-                }
-                if (Object.prototype.hasOwnProperty.call(dataset[EXPANDO_KEY], 'borderColor')) {
-                    dataset.borderColor = dataset[EXPANDO_KEY].borderColor;
+        chart.config.data.datasets.forEach((dataset: ChartDataset<ChartType>) => {
+            if ((dataset as any)[EXPANDO_KEY]) {
+                if (
+                    Object.prototype.hasOwnProperty.call(
+                        (dataset as any)[EXPANDO_KEY],
+                        'backgroundColor'
+                    )
+                ) {
+                    dataset.backgroundColor = (dataset as any)[EXPANDO_KEY].backgroundColor;
                 }
                 if (
                     Object.prototype.hasOwnProperty.call(
-                        dataset[EXPANDO_KEY],
+                        (dataset as any)[EXPANDO_KEY],
+                        'borderColor'
+                    )
+                ) {
+                    dataset.borderColor = (dataset as any)[EXPANDO_KEY].borderColor;
+                }
+                if (
+                    Object.prototype.hasOwnProperty.call(
+                        (dataset as any)[EXPANDO_KEY],
                         'pointBackgroundColor'
                     )
                 ) {
-                    dataset.pointBackgroundColor = dataset[EXPANDO_KEY].pointBackgroundColor;
+                    (dataset as any).pointBackgroundColor = (dataset as any)[
+                        EXPANDO_KEY
+                    ].pointBackgroundColor;
                 }
                 if (
-                    Object.prototype.hasOwnProperty.call(dataset[EXPANDO_KEY], 'pointBorderColor')
+                    Object.prototype.hasOwnProperty.call(
+                        (dataset as any)[EXPANDO_KEY],
+                        'pointBorderColor'
+                    )
                 ) {
-                    dataset.pointBorderColor = dataset[EXPANDO_KEY].pointBorderColor;
+                    (dataset as any).pointBorderColor = (dataset as any)[
+                        EXPANDO_KEY
+                    ].pointBorderColor;
                 }
-                delete dataset[EXPANDO_KEY];
+                delete (dataset as any)[EXPANDO_KEY];
             }
         });
     },
