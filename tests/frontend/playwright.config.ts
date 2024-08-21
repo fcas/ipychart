78367495f16ab9e1aba1e8e9bf407be14b3a799b
Nicolas Houlier <nicolas.houlier@gmail.com>
2024-08-21 17:12:45 +0200

    Fix playwright webserver not launching in a Docker environment

--- tests/frontend/playwright.config.ts
+++ tests/frontend/playwright.config.ts
@@ -4,7 +4,7 @@ module.exports = {
     ...baseConfig,
     timeout: 240000,
     webServer: {
-        command: 'jlpm start',
+        command: 'jlpm start --allow-root',
         port: '8888',
         timeout: 120 * 1000,
         reuseExistingServer: !process.env.CI,
