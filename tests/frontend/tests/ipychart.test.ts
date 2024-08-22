66f5cb84328e41ef8c5c57b045ceca77c6dca395
Nicolas Houlier <nicolas.houlier@gmail.com>
2024-08-22 10:06:13 +0200

    Use one separate frontend test per cell for tests results clarity

--- tests/frontend/tests/ipychart.test.ts
+++ tests/frontend/tests/ipychart.test.ts
@@ -12,38 +12,48 @@ async function sleep(time: number) {
 }
 
 test.describe('Widget Visual Regression', () => {
+    let captures: Buffer[] = [];
+    let cellCount: number | undefined;
+    const notebook = 'ipychart-test-notebook.ipynb';
+
     test.beforeEach(async ({ page, tmpPath }) => {
         await page.contents.uploadDirectory(path.resolve(__dirname, './notebooks'), tmpPath);
         await page.filebrowser.openDirectory(tmpPath);
     });
 
-    test('Run notebook ipychart-test-notebook.ipynb and capture cell outputs', async ({
-        page,
-        tmpPath,
-    }) => {
-        const notebook = 'ipychart-test-notebook.ipynb';
+    test('Run notebook ipychart-test-notebook.ipynb and capture cell outputs', async ({ page, tmpPath }) => {
         await page.notebook.openByPath(`${tmpPath}/${notebook}`);
         await page.notebook.activate(notebook);
-        await page.waitForTimeout(500)
+        await page.waitForTimeout(500);
 
-        const captures = new Array<Buffer>();
-        const cellCount = await page.notebook.getCellCount();
+        cellCount = await page.notebook.getCellCount();
+
+        captures = new Array<Buffer>();
 
         await page.notebook.runCellByCell({
             onAfterCellRun: async (cellIndex: number) => {
                 const cell = await page.notebook.getCellOutputLocator(cellIndex);
                 if (cell) {
                     await sleep(1000);
-                    captures.push(await cell.screenshot());
+                    captures.push(await cell.screenshot({ animations: 'disabled' }));
                 }
             },
         });
 
         await page.notebook.save();
-
-        for (let i = 0; i < cellCount; i++) {
-            const image = `${notebook}-cell-${i}.png`;
-            expect.soft(captures[i]).toMatchSnapshot(image, { threshold: 0.5, maxDiffPixelRatio: 0.03 });
-        }
     });
+
+    // Adjust the maximum number of cells as needed
+    for (let i = 0; i < 25; i++) {
+        test(`Compare output of cell ${i}`, async () => {
+            if (typeof cellCount === 'undefined' || i >= cellCount) {
+                test.skip();
+            } else {
+                const image = `${notebook}-cell-${i}.png`;
+                expect
+                    .soft(captures[i])
+                    .toMatchSnapshot(image, { threshold: 0.5, maxDiffPixelRatio: 0.03 });
+            }
+        });
+    }
 });
